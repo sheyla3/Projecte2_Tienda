f539f2c534838923375f13c82ce4b261916f0ec3
yeeriis <gebeal2002@gmail.com>
2023-12-11 14:01:38 +0100

    CSS Done

--- controllers/PrincipalController.php
+++ controllers/PrincipalController.php
@@ -6,11 +6,13 @@ class PrincipalController
 {
     public function mostrarPaginaPrincipal()
     {
+        require_once "views/general/slider.php";
         $categoria = new Categoria();
         $listaCategorias = $categoria->obtenerCategorias();
         require_once "views/general/mostrarListaCategorias.php";
         $producto = new Producto();
         $destacados = $producto->productoDestacado();
         require_once "views/general/mostrarDestacados.php";
+        
     }
 }
---
+++ views/general/slider.php
@@ -0,0 +1,29 @@
+<div class="slider">
+    <?php
+    $ids = array(1, 2, 3, 4, 5);
+    $alt = array(
+        "Slide 1",
+        "Slide 2",
+        "Slide 3",
+        "Slide 4",
+        "Slide 5"
+    );
+    $max = count($ids);
+    for ($s = 0; $s < $max; $s++) { ?>
+        <input type="radio" id="<?= $ids[$s]; ?>" name="image-slide" hidden />
+    <?php } ?>
+    <div class="slideshow">
+        <?php for ($s = 0; $s < $max; $s++) { ?>
+            <div class="item-slide">
+                <img src="img/<?= $ids[$s]; ?>.jpg" alt="<?= $alt[$s]; ?>" />
+            </div>
+        <?php } ?>
+    </div>
+    <div class="pagination">
+        <?php for ($s = 0; $s < $max; $s++) { ?>
+            <label class="pag-item" for="<?= $ids[$s]; ?>">
+                <img src="./img/<?= $ids[$s]; ?>.jpg" alt="<?= $alt[$s]; ?>" />
+            </label>
+        <?php } ?>
+    </div>
+</div>
