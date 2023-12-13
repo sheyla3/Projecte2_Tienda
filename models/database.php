8c63f48b8cbbe7309352a574ee7ccbd0a67fb086
joel-casado <joel.casado@inslapineda.cat>
2023-12-13 11:40:20 +0100

    Archivo borrado

--- models/database.php
+++ models/database.php
@@ -8,7 +8,7 @@ class Database{
         $servername = "localhost";
         $dbname= "srg";
         $username = "postgres";
-        $password = "1234";
+        $password = "postgre";
 
         try{
         //Creem una nova connexió instancinat l'objecte PDO
