1d209b36eb577e3ed8774de165808a5b20c5c36f
joel-casado <joel.casado@inslapineda.cat>
2023-12-13 10:18:55 +0100

    Creación de usuario

---
+++ models/database.php
@@ -0,0 +1,33 @@
+<?php
+
+class Database{
+    protected $db;
+    
+    
+    public function __construct(){
+        $servername = "localhost";
+        $dbname= "srg";
+        $username = "postgres";
+        $password = "123";
+
+        try{
+        //Creem una nova connexió instancinat l'objecte PDO
+		$this->db = new PDO("pgsql:host=$servername;dbname=$dbname",$username, $password);
+		// Establim el mode PDO error a exception per poder recuperar les excepccions
+		$this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        }
+        catch(PDOException $error)
+        {
+            echo "Connection failed: ". $error->getMessage();
+            
+        }
+        
+        
+    }
+    public function getDB() {
+        return $this->db;
+    }
+
+}
+
