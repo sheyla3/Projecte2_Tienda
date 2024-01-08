59eba99e8bf4eee7e6efbb8de9c1760b7b200c79
sheyla3 <144891028+sheyla3@users.noreply.github.com>
2024-01-08 13:22:34 +0100

    Add files via upload

---
+++ views/general/cabezeraSesion.php
@@ -0,0 +1,86 @@
+<header>
+    <nav>
+        <div class="header">
+            <div class="logo"><img src="views/img/logo.png" alt="SRG" width="200" height="70"></div>
+            <input type="radio" name="slider" id="menu-btn">
+            <input type="radio" name="slider" id="close-btn">
+            <ul class="nav-links">
+                <label for="close-btn" class="btn close-btn"><img src="views/img/cerrar_icono.png" alt="cerrar" width="20" height="20"></label>
+                <li>
+                    <a href="#" class="tipo">Mujer</a>
+                    <input type="checkbox" id="showMega">
+                    <label for="showMega" class="mobile-item">Mujer</label>
+                    <div class="mega-box">
+                        <div class="contenido">
+                            <div class="fila">
+                                <img src="views/img/zapatos_mujer.png" alt="Zapatos Mujer">
+                            </div>
+                            <div class="fila">
+                                <ul class="categorias">
+                                    <li><a href="#">Piel</a></li>
+                                    <li><a href="#">Sandalias planas</a></li>
+                                    <li><a href="#">Sandalias tacón</a></li>
+                                    <li><a href="#">Deportivas</a></li>
+                                </ul>
+                            </div>
+                            <div class="fila">
+                                <ul class="categorias">
+                                    <li><a href="#">Zapatos tacón</a></li>
+                                    <li><a href="#">Zapatos planos</a></li>
+                                    <li><a href="#">Botas y botines</a></li>
+                                    <li><a href="#">Fiesta</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <a href="#" class="tipo">Hombre</a>
+                    <input type="checkbox" id="showMega2">
+                    <label for="showMega2" class="mobile-item">Hombre</label>
+                    <div class="mega-box">
+                        <div class="contenido">
+                            <div class="fila">
+                                <img src="views/img/zapatos_hombre.png" alt="Zapatos hombre">
+                            </div>
+                            <div class="fila">
+                                <ul class="categorias">
+                                    <li><a href="#">Botas y botines</a></li>
+                                    <li><a href="#">Deportivas</a></li>
+                                    <li><a href="#">Mocasines</a></li>
+                                </ul>
+                            </div>
+                            <div class="fila">
+                                <ul class="categorias">
+                                    <li><a href="#">Piel</a></li>
+                                    <li><a href="#">Zapatos de vestir</a></li>
+                                    <li><a href="#">Zapatos casuales</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <a href="#" class="tipo"><img src="views/img/lupa.png" alt="Buscar" width="20" height="20"></a>
+                    <label class="mobile-item">Buscar</label>
+                </li>
+                <li>
+                    <a href="#" class="tipo"><img src="views/img/carrito.png" alt="Carrito" width="20" height="20"></a>
+                    <label class="mobile-item">Carrito</label>
+                </li>
+                <li>
+                    <a href="#" class="tipo"><img src="views/img/usuario.png" alt="Iniciar sesión" width="20" height="20"></a>
+                    <label class="mobile-item">Usuario</label>
+                    <input type="checkbox" id="showDrop">
+                    <ul class="drop-menu">
+                        <li><a href="#">Carrito</a></li>
+                        <li><a href="#">Mis compras</a></li>
+                        <li><a href="#">Favoritos</a></li>
+                        <li><a href="#">Cerrar sesión</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <label for="menu-btn" class="btn menu-btn"><img src="views/img/menu_icono.png" alt="menu" width="20" height="20"></label>
+        </div>
+    </nav>
+</header>
