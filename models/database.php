abf26d9bcb5832856f30c2431f90dbda63358b0c
yeeriis <gebeal2002@gmail.com>
2024-01-12 10:22:12 +0100

    Profile Done

--- models/database.php
+++ models/database.php
@@ -8,7 +8,7 @@ class Database{
         $servername = "localhost";
         $dbname= "srg";
         $username = "postgres";
-        $password = "123";
+        $password = "admin1234";
 
         try{
         //Creem una nova connexió instancinat l'objecte PDO
