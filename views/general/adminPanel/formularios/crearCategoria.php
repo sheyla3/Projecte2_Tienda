05dd6323cb2049944e6e67cec2ed983273d69f05
sheyla3 <144891028+sheyla3@users.noreply.github.com>
2024-01-24 09:22:53 +0100

    Update crearCategoria.php

--- views/general/adminPanel/formularios/crearCategoria.php
+++ views/general/adminPanel/formularios/crearCategoria.php
@@ -11,11 +11,7 @@ echo "
         <select id='genero' name='genero'>
             <option value='Hombre'>Hombre</option>
             <option value='Mujer'>Mujer</option>
-        </select>
-
-       
-	    <input type='checkbox' id='estado' name='estado' checked>
-	    <label for='estado'>Estado</label><br>
+        </select><br>
         <a href='index.php?controller=Admin&action=botonVistaCategoria' class='admin-panel-submit-link'>Volver atras</a>
 
         <input class='admin-panel-submit-link' type='submit' value='AÑADIR'>
